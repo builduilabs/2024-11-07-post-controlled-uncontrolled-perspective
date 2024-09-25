b5ba1a923ea0603f447a99701e324a81f3183037
Sam Selikoff <sam.selikoff@gmail.com>
2024-09-25 01:13:01 -0400

    wip

--- src/app/demo-1/page.tsx
+++ src/app/demo-1/page.tsx
@@ -1,92 +1,43 @@
 'use client';
 
-import { MagnifyingGlassIcon } from '@heroicons/react/16/solid';
-import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
-import { Response } from '../api/people/route';
+import { Text } from '../components/text';
+import { Input } from '../components/input';
 import { Heading } from '../components/heading';
-import { Input, InputGroup } from '../components/input';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '../components/table';
-import Spinner from '../spinner';
-import { Strong, Text } from '../components/text';
+import { Button } from '../components/button';
 
-export default function Home() {
+export default function Page() {
   let [search, setSearch] = useState('');
-  let { data, isPlaceholderData } = useQuery({
-    queryKey: ['people', search],
-    queryFn: async () => {
-      let res = await fetch(`/api/people?search=${search}`);
-      let data = await res.json();
-
-      return data as Response;
-    },
-    placeholderData: (previousData) => previousData,
-  });
 
   return (
     <>
       <span data-hide-urlbar />
-      <Heading>Your team</Heading>
+      <div className="grow flex">
+        <div className="w-1/2 border-r items-center pr-6">
+          <Heading className="text-center">Uncontrolled</Heading>
 
-      {!data ? (
-        <div className="mt-20 flex justify-center">
-          <Spinner className="size-5" />
-        </div>
-      ) : (
-        <>
-          <div className="mt-4 grid sm:grid-cols-2 gap-4 items-center">
-            <div>
-              <InputGroup>
-                {isPlaceholderData ? (
-                  <Spinner data-slot="icon" />
-                ) : (
-                  <MagnifyingGlassIcon />
-                )}
-                <Input
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                  placeholder="Find someone&hellip;"
-                  name="search"
-                  aria-label="Search"
-                />
-              </InputGroup>
-            </div>
-            <div className="sm:text-right">
-              <Text className="text-sm/6">
-                Showing <Strong>{data.meta.current}</Strong> of{' '}
-                <Strong>{data.meta.total}</Strong> results
-              </Text>
-            </div>
+          <div className="space-y-4 mt-10">
+            <Input name="uncontrolled" />
+            <Text>React state:</Text>
           </div>
-          <Table dense className="mt-4">
-            <TableHead>
-              <TableRow>
-                <TableHeader>Name</TableHeader>
-                <TableHeader>Email</TableHeader>
-                <TableHeader>Role</TableHeader>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.people.map((person) => (
-                <TableRow key={person.id}>
-                  <TableCell className="font-medium">{person.name}</TableCell>
-                  <TableCell className="text-gray-500">
-                    {person.email}
-                  </TableCell>
-                  <TableCell className="text-gray-500">{person.role}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </>
-      )}
+        </div>
+        <div className="w-1/2 pl-6">
+          <Heading className="text-center">Controlled</Heading>
+
+          <div className="space-y-4 mt-10">
+            <Input
+              name="controlled"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <Text className="truncate">React state: {search}</Text>
+
+            <Button onClick={() => setSearch('')} outline>
+              Clear search
+            </Button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/app/demo-2/page.tsx
+++ src/app/demo-2/page.tsx
@@ -1,84 +1,48 @@
 'use client';
 
-import { MagnifyingGlassIcon } from '@heroicons/react/16/solid';
-import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
-import { Response } from '../api/people/route';
+import Counter from './counter';
+import { Text } from '../components/text';
 import { Heading } from '../components/heading';
-import { Input, InputGroup } from '../components/input';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '../components/table';
-import Spinner from '../spinner';
+import { Button } from '../components/button';
 
-export default function Home() {
-  let [search, setSearch] = useState('');
-  let { data, isPlaceholderData } = useQuery({
-    queryKey: ['people', search],
-    queryFn: async () => {
-      let res = await fetch(`/api/people?search=${search}`);
-      let data = await res.json();
-
-      return data as Response;
-    },
-    placeholderData: (previousData) => previousData,
-  });
+export default function Page() {
+  let [count, setCount] = useState(0);
 
   return (
     <>
-      <Heading>Your team</Heading>
+      <span data-hide-urlbar />
+      <div className="grow flex">
+        <div className="w-1/2 border-r pr-6">
+          <Heading className="text-center">Uncontrolled</Heading>
 
-      {!data ? (
-        <div className="mt-20 flex justify-center">
-          <Spinner className="size-5" />
+          <div className="mt-10 flex justify-center">
+            <Counter />
+          </div>
+
+          <div className="mt-4">
+            <Text>App&apos;s state:</Text>
+          </div>
         </div>
-      ) : (
-        <>
-          <div className="mt-4 grid sm:grid-cols-2 gap-4 items-center">
-            <div>
-              <InputGroup>
-                {isPlaceholderData ? (
-                  <Spinner data-slot="icon" />
-                ) : (
-                  <MagnifyingGlassIcon />
-                )}
-                <Input
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                  placeholder="Find someone&hellip;"
-                  name="search"
-                  aria-label="Search"
-                />
-              </InputGroup>
+        <div className="w-1/2 pl-6">
+          <Heading className="text-center">Controlled</Heading>
+
+          <div className="mt-10">
+            <div className="mt-10 flex justify-center">
+              <Counter value={count} onChange={setCount} />
+            </div>
+
+            <div className="mt-4 space-y-4">
+              <Text className="truncate">
+                App&apos;s state: <span className="tabular-nums">{count}</span>
+              </Text>
+              <Button onClick={() => setCount(0)} outline>
+                Reset count
+              </Button>
             </div>
           </div>
-          <Table dense className="mt-4">
-            <TableHead>
-              <TableRow>
-                <TableHeader>Name</TableHeader>
-                <TableHeader>Email</TableHeader>
-                <TableHeader>Role</TableHeader>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.people.map((person) => (
-                <TableRow key={person.id}>
-                  <TableCell className="font-medium">{person.name}</TableCell>
-                  <TableCell className="text-gray-500">
-                    {person.email}
-                  </TableCell>
-                  <TableCell className="text-gray-500">{person.role}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </>
-      )}
+        </div>
+      </div>
     </>
   );
 }
