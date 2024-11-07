8bfad9679b800064686609236b39b10a95c96df6
Sam Selikoff <sam.selikoff@gmail.com>
2024-11-07 13:51:16 -0500

    wip

--- src/app/demo-7/page.tsx
+++ src/app/demo-7/page.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import Counter from './counter';
-import { Text } from '../components/text';
-import { Heading } from '../components/heading';
-import { Button } from '../components/button';
+import { useState } from "react";
+import Counter from "./counter";
+import { Text } from "../components/text";
+import { Heading } from "../components/heading";
+import { Button } from "../components/button";
 
 export default function Page() {
   let [count, setCount] = useState(0);
@@ -12,8 +12,8 @@ export default function Page() {
   return (
     <>
       <span data-hide-urlbar />
-      <div className="grow flex">
-        <div className="w-1/2 border-r pr-6">
+      <div className="flex grow flex-col sm:flex-row">
+        <div className="h-1/2 border-b pt-6 sm:h-auto sm:w-1/2 sm:border-b-0 sm:border-r sm:pr-6 sm:pt-0">
           <Heading className="text-center">Uncontrolled</Heading>
 
           <div className="mt-10 flex justify-center">
@@ -24,7 +24,8 @@ export default function Page() {
             <Text>App&apos;s state:</Text>
           </div>
         </div>
-        <div className="w-1/2 pl-6">
+
+        <div className="h-1/2 pt-6 sm:h-auto sm:w-1/2 sm:pl-6 sm:pt-0">
           <Heading className="text-center">Controlled</Heading>
 
           <div className="mt-10">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,18 @@
-import type { Config } from 'tailwindcss';
-import colors from 'tailwindcss/colors';
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
-  darkMode: 'selector',
+  darkMode: "selector",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      screens: {
+        sm: "608px",
+      },
       colors: {
         gray: colors.zinc,
       },
